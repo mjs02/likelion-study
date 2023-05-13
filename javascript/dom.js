cf40913f6762383ce375c2fe2f47554aa7462caa
mjs02 <127651767+mjs02@users.noreply.github.com>
2023-05-14 02:02:54 +0900

    Add files via upload

---
+++ javascript/dom.js
@@ -0,0 +1,13 @@
+//document.getElementById("brand-title");
+
+//console.log(el);
+
+//console.log(el.innerHTML);
+//console.log(el.innerText);
+
+
+//el.innerText = "안녕하세요 :)"
+
+var el = document.getElementsByClassName('sub-title')
+
+console.log(el);
